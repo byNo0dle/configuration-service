c14c1cfe065bfb00cd7e03993ebc8b1bb350f881
Nick <nicksaurinocruz@gmail.com>
2024-02-02 17:26:20 -0500

    The necessary dependencies were added to be able to start with this project

---
+++ src/main/java/com/ufostyle/configurationservice/BankConfigurationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ufostyle.configurationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankConfigurationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankConfigurationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ufostyle/configurationservice/BankConfigurationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufostyle.configurationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankConfigurationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
