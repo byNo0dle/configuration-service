78c064e8718724d36b70a8c4fd5f0a4404423231
Nick <nicksaurinocruz@gmail.com>
2024-02-05 09:37:56 -0500

    Configuration class added

---
+++ src/main/java/com/ufostyle/configurationservice/entities/Configuration.java
@@ -0,0 +1,30 @@
+package com.ufostyle.configurationservice.entities;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Builder
+@Data
+@Document(collection = "configurations")
+public class Configuration {
+
+  @Id
+  String idConfiguration;
+  Double costMaintenance;
+  Integer quantityMovement;
+  Integer quantityCredit;
+  String specifyDate;
+
+  @Override
+  public String toString() {
+    return "Configuration [" +
+        "idConfiguration='" + idConfiguration + '\'' +
+        ", costMaintenance=" + costMaintenance +
+        ", quantityMovement=" + quantityMovement +
+        ", quantityCredit=" + quantityCredit +
+        ", specifyDate='" + specifyDate + '\'' +
+        "]";
+  }
+}
